b94dd878326eeff5d622e07ed957d8a10054b182
vegardnb <vegard@blomvik.net>
2015-12-27 21:36:37 +0100

    Vegard made a change

--- Platform/src/main/Main.java
+++ Platform/src/main/Main.java
@@ -7,11 +7,13 @@ public class Main {
 		Controller c = new Controller();
 		UserInterface ui = new UserInterface(c);
 		
+		
 		System.out.println("test 1");
 		System.out.println("test 2");
 		System.out.println("test 3 fra Astrids MacBook Air");
 		System.out.println("test 4 fra Brynjars MBP");
-		
+		System.out.println("test 5 MBP Åse");
+			
 		
 		
 	}
